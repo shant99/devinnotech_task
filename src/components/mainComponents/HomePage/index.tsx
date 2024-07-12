115511ca891610231509c163283542084d910f88
Shant Hakobyan <73386600+shant99@users.noreply.github.com>
2024-07-12 05:53:52 -0700

    Add files via upload

---
+++ src/components/mainComponents/HomePage/index.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import { constants } from "../../../assets/constants";
+import { useAppSelector } from "../../../store/hooks";
+import styles from "./home_page.module.css";
+
+const HomePage = () => {
+  const { paths } = constants;
+  const { currentWeatherData } = useAppSelector((state) => state.weather);
+
+  return (
+    <div className={styles.home_page_wrapper}>
+      <div className={styles.home_page_main_info}>
+        <p className={styles.home_page_welcome}>Welcome to our page</p>
+        <p className={styles.home_page_info}>
+          To see the daily weather please click on bellow button
+        </p>
+        <Link to={paths.weather} className={styles.home_page_btn}>
+          See daily Weather
+        </Link>
+      </div>
+      <div className={styles.current_weather_wrapper}>
+        <p>{currentWeatherData.name}</p>
+        <p>{currentWeatherData.main.temp.toFixed(1)} &#8451;</p>
+        {currentWeatherData.weather[0].icon && (
+          <img
+            src={`https://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png`}
+            alt="icon"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
